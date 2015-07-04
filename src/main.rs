905e50cdc514af6ab9cdf2ea48b14162aeee825d
valentin <valentin.rigolle@gmail.com>
2015-07-04 22:50:10 +0200

    Added `draw` command help.

--- src/main.rs
+++ src/main.rs
@@ -25,77 +25,7 @@ fn main() {
             _          => print_help(""),
         }
     }
-
-    return;
-
-    let mut window = RenderWindow::new(VideoMode::new_init(900, 600, 32),
-                                   "Mandelbrot",
-                                   window_style::CLOSE,
-                                   &ContextSettings::default()).expect("Couldn't create RenderWindow");
-    window.set_framerate_limit(30);
-
-    let mut redraw = false; //should we redraw the set ?
-    let mut plan = Plan {up: -1.0, left: -2.0, width: 3.0, height: 2.0};
-    let image_dim = ImageDim {width: 900, height: 600};
-    let max_iter = 100;
-    let mut img = draw_mandelbrot(&Color::new_rgb(0, 0, 0),
-                    &plan,
-                    &image_dim,
-                    max_iter);
-    let mut tex = Texture::new_from_image(&img).unwrap();
-
-    let mut zoom_lvl = 0;
-    let mut rect = RectangleShape::new_init(&Vector2f::new(90., 60.)).unwrap();
-    rect.set_origin(&Vector2f::new(45., 30.));
-    rect.set_fill_color(&Color::new_rgba(0, 0, 0, 0));
-    rect.set_outline_color(&Color::new_rgb(255, 255, 255));
-    rect.set_outline_thickness(1.);
-
-    while window.is_open() {
-        for event in window.events() {
-            match event {
-                event::Closed               => window.close(),
-                event::MouseButtonPressed {
-                    button, x, y
-                }                           => {
-                                                   if button == MouseButton::Right {
-                                                       zoom_lvl = 0;
-                                                       redraw = true;
-                                                       plan = Plan {up: -1.0, left: -2.0, width: 3.0, height: 2.0}; //valeurs par défaut
-                                                   } else if button == MouseButton::Left {
-                                                       redraw = true;
-                                                       //zoom sur un rectangle de 90*60 centré sur la souris
-                                                       let left = x - 45;
-                                                       let up = y - 30;
-                                                       let up_left = scale(left, up, &image_dim, &plan);
-                                                       let width_height = scale(90, 60, &image_dim, &Plan {up: 0., left: 0., width: plan.width, height: plan.height});
-                                                       plan = Plan {up: up_left.im(), left: up_left.re(), width: width_height.re(), height: width_height.im()};
-                                                       zoom_lvl += 1;
-                                                       println!("ZOOM: {} sur ({:.5}; {:.5})", zoom_lvl, plan.left+plan.width/2., plan.up+plan.height/2.);
-                                                   }
-                                               },
-                event::MouseMoved {
-                    x, y
-                }                           => rect.set_position(&Vector2f::new(x as f32, y as f32)),
-                _                           => {},
-            }
-        }
-
-        if redraw {
-            img = draw_mandelbrot(&Color::new_rgb(0, 0, 0),
-                            &plan,
-                            &image_dim,
-                            max_iter*zoom_lvl+60);
-            tex = Texture::new_from_image(&img).unwrap();
-            //sprite = Sprite::new_with_texture(&tex).unwrap();
-            redraw = false;
-        }
-
-        window.clear(&Color::black());
-        window.draw(&Sprite::new_with_texture(&tex).unwrap());
-        window.draw(&rect);
-        window.display();
-    }
+    println!("");
 }
 
 #[derive(Debug)]
@@ -257,13 +187,22 @@ fn print_help(category: &str) {
         "generate" => {
             println!("RUST-MANDELBROT : GENERATE");
             println!("--------------------------\n");
-            println!("Synopsis : generate [plan] [image] [max iter] [file]\n");
-            println!("  plan : the frame of the mandelbrot set you want to draw. It must be of the form 'up,left,width,height'. If you want the default settings (that are '-1,-2,3,2'), just type '~'.");
-            println!("  image : the image size, in pixels. It must match the following pattern : 'width,height'. If you want the default size (that is '900, 600'), type '~'.");
-            println!("  max iter : the max iterations used to determine the set's points. If you don't know which value you should use, prefer a ");
-            println!("  file : the output file to write the image. The format will be guessed from the extension. Supported fromats are : bmp, png, tga and jpg.");
+            println!("Synopsis : generate [plan] [image] [max_iter] [file]\n");
+            println!("  plan     : the frame of the mandelbrot set you want to draw. It must be of the form 'up,left,width,height'. If you want the default settings (that are '-1,-2,3,2'), just type '~'.");
+            println!("  image    : the image size, in pixels. It must match the following pattern : 'width,height'. If you want the default size (that is '900, 600'), type '~'.");
+            println!("  max_iter : the max iterations used to determine the set's points. If you don't know which value you should use, prefer a number around 100.");
+            println!("  file     : the output file to write the image. The format will be guessed from the extension. Supported fromats are : bmp, png, tga and jpg.");
             println!("--------------------------");
         },
+        "draw" => {
+            println!("RUST-MANDELBROT : DRAW");
+            println!("----------------------\n");
+            println!("Synopsis : draw [plan] [window] [max_iter]\n");
+            println!("  plan     : the frame of the mandelbrot set you want to use as default view. When you will right-click, it will bring you to this view. It must be of the form 'up,left,width,height'. If you want the default settings (that are '-1,-2,3,2'), just type '~'.");
+            println!("  window   : the size of the window, in pixels. It must match the following pattern : 'width,height'. If you want the default size (that is '900, 600'), type '~'.");
+            println!("  max_iter : the max iterations used to determine the set's points. If you don't know which value you should use, prefer a number around 100.");
+            println!("--------------------------");
+        }
         _ => {
             println!("RUST-MANDELBROT : USE");
             println!("---------------------\n\nYou must specify a command while calling the programm :");
@@ -277,7 +216,7 @@ fn print_help(category: &str) {
 
 fn generate_mandelbrot(args: Vec<String>) {
     if args.len() != 6 {
-        println!("Error : the `generate` command requires 2 arguments");
+        println!("Error : the `generate` command requires 4 arguments");
         println!("See `help generate` to get specific help");
     } else {
         let option_plan = Plan::from_string(&args[2]);
@@ -316,6 +255,84 @@ fn generate_mandelbrot(args: Vec<String>) {
     }
 }
 
-fn draw_main(_args: Vec<String>) {
+fn draw_main(args: Vec<String>) {
+    if args.len() != 5 {
+        println!("");
+        println!("Error : the `draw` command requires 3 arguments");
+        println!("See `help draw` to get specific help");
+    } else {
+        let option_plan = Plan::from_string(&args[2]);
+        if let Some(mut plan) = option_plan {
+            let option_image = ImageDim::from_string(&args[3]);
+            if let Some(mut image_dim) = option_image {
+                //Dessin :)
+                if let Ok(mut max_iter) = args[4].parse::<u32>() {
+                    let mut window = RenderWindow::new(VideoMode::new_init(900, 600, 32),
+                                                   "Mandelbrot",
+                                                   window_style::CLOSE,
+                                                   &ContextSettings::default()).expect("Couldn't create RenderWindow");
+                    window.set_framerate_limit(30);
 
+                    let mut redraw = false; //should we redraw the set ?
+                    let mut img = draw_mandelbrot(&Color::new_rgb(0, 0, 0),
+                                    &plan,
+                                    &image_dim,
+                                    max_iter);
+                    let mut tex = Texture::new_from_image(&img).unwrap();
+
+                    let mut zoom_lvl = 0;
+                    let mut rect = RectangleShape::new_init(&Vector2f::new(90., 60.)).unwrap();
+                    rect.set_origin(&Vector2f::new(45., 30.));
+                    rect.set_fill_color(&Color::new_rgba(0, 0, 0, 0));
+                    rect.set_outline_color(&Color::new_rgb(255, 255, 255));
+                    rect.set_outline_thickness(1.);
+
+                    while window.is_open() {
+                        for event in window.events() {
+                            match event {
+                                event::Closed               => window.close(),
+                                event::MouseButtonPressed {
+                                    button, x, y
+                                }                           => {
+                                                                   if button == MouseButton::Right {
+                                                                       zoom_lvl = 0;
+                                                                       redraw = true;
+                                                                       plan = Plan {up: -1.0, left: -2.0, width: 3.0, height: 2.0}; //valeurs par défaut
+                                                                   } else if button == MouseButton::Left {
+                                                                       redraw = true;
+                                                                       //zoom sur un rectangle de 90*60 centré sur la souris
+                                                                       let left = x - 45;
+                                                                       let up = y - 30;
+                                                                       let up_left = scale(left, up, &image_dim, &plan);
+                                                                       let width_height = scale(90, 60, &image_dim, &Plan {up: 0., left: 0., width: plan.width, height: plan.height});
+                                                                       plan = Plan {up: up_left.im(), left: up_left.re(), width: width_height.re(), height: width_height.im()};
+                                                                       zoom_lvl += 1;
+                                                                       println!("ZOOM: {} sur ({:.5}; {:.5})", zoom_lvl, plan.left+plan.width/2., plan.up+plan.height/2.);
+                                                                   }
+                                                               },
+                                event::MouseMoved {
+                                    x, y
+                                }                           => rect.set_position(&Vector2f::new(x as f32, y as f32)),
+                                _                           => {},
+                            }
+                        }
+
+                        if redraw {
+                            img = draw_mandelbrot(&Color::new_rgb(0, 0, 0),
+                                            &plan,
+                                            &image_dim,
+                                            max_iter*zoom_lvl+60);
+                            tex = Texture::new_from_image(&img).unwrap();
+                            redraw = false;
+                        }
+
+                        window.clear(&Color::black());
+                        window.draw(&Sprite::new_with_texture(&tex).unwrap());
+                        window.draw(&rect);
+                        window.display();
+                    }
+                }
+            }
+        }
+    }
 }
